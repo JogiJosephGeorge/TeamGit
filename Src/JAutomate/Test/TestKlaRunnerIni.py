ea78739d8ca37d114c75d329c6f2708e4154e8ae
Jogi Joseph George <Jogi.George@kla-tencor.com>
2021-02-01 23:31:46 +0530

    Version 1.3.8 Extended unit tests for KlaRunner.ini file

--- Src/JAutomate/Test/TestKlaRunnerIni.py
+++ Src/JAutomate/Test/TestKlaRunnerIni.py
@@ -18,9 +18,20 @@ class TestKlaRunnerIni:
             src = srcSet[0]
             Test.Assert(os.path.isdir(src), True, 'Directory {} exists.'.format(src))
         self.TestIndex(self.model.Sources, self.model.SrcIndex, 'Index')
+        srcCnt = len(self.model.Sources)
+        for activeSrc in self.model.ActiveSrcs:
+            Test.Assert(activeSrc >= 0 and activeSrc < srcCnt, True, 'Invalid Active Src {} is given'.format(activeSrc))
 
     def AutoTest(self):
+        self.VerifyNamesAndSlots(self.model.AutoTests.Tests)
         self.TestIndex(self.model.AutoTests.Tests, self.model.TestIndex, 'Index')
+
+    def VerifyNamesAndSlots(self, list):
+        for testName, slots in list:
+            Test.Assert(len(testName) > 0, True, 'Test Name : ' + testName, 1)
+            Test.Assert(len(slots) > 0, True, 'Slots selected for ' + testName, 1)
+            for slot in slots:
+                Test.Assert(slot > 0 and slot <= self.model.MaxSlots, True, 'Test Name {} Slot {}'.format(testName, slot), 1)
 
     def TestIndex(self, list, index, message):
         isValidIndex = index >= 0 and index < len(list)
