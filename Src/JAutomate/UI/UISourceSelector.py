c3e7c674f45282bb7f6e43366195745770b03195
Jogi Joseph George <Jogi.George@kla-tencor.com>
2021-02-02 02:51:30 +0530

    Version 1.3.10 New class has been added for managing Grid alone

--- Src/JAutomate/UI/UISourceSelector.py
+++ Src/JAutomate/UI/UISourceSelector.py
@@ -12,9 +12,123 @@ from UI.UIWindow import UIWindow
 from Common.PrettyTable import PrettyTable, TableFormat
 
 
+class UISourceGrid:
+    def __init__(self, model, parentFrame):
+        self.model = model
+        self.ParentFrame = parentFrame
+        self.chkActiveSrcs = []
+        self.radCurSrc = []
+        self.lblBranches = []
+        self.cboConfig = []
+        self.cboPlatform = []
+        self.SelectedSrc = tk.IntVar()
+        self.SelectedSrc.set(self.model.SrcIndex)
+
+    def CreateUI(self):
+        self.AddHeader()
+        r = 1
+        for srcTuple in self.model.Sources:
+            self.AddSrcRow(r, srcTuple)
+            r += 1
+
+    def AddHeader(self):
+        UIFactory.AddLabel(self.ParentFrame, 'Is Active', 0, 0)
+        UIFactory.AddLabel(self.ParentFrame, 'Current Source', 0, 1)
+        UIFactory.AddLabel(self.ParentFrame, 'Branch', 0, 2)
+        UIFactory.AddLabel(self.ParentFrame, 'Configuration', 0, 3)
+        UIFactory.AddLabel(self.ParentFrame, 'Platform', 0, 4)
+
+    def AddSrcRow(self, r, srcTuple):
+        self.AddActive(r, 0)
+        self.AddSource(r, 1, srcTuple[0])
+        self.AddBranch(r, 2)
+        self.AddConfig(r, 3, srcTuple[1])
+        self.AddPlatform(r, 4, srcTuple[2])
+
+    def AddActive(self, r, c):
+        Index = r - 1
+        isActive = Index in self.model.ActiveSrcs
+        chk = UIFactory.AddCheckBox(self.ParentFrame, '', isActive, r, c, self.OnActiveSrcChanged, (Index,), 'ew')
+        self.chkActiveSrcs.append(chk)
+
+    def OnActiveSrcChanged(self, Index):
+        if self.IsSrcActive(Index):
+            self.model.ActiveSrcs.append(Index)
+            self.model.ActiveSrcs = list(set(self.model.ActiveSrcs))
+            print 'Enabled the source : ' + str(self.model.Sources[Index][0])
+        else:
+            self.model.ActiveSrcs.remove(Index)
+            print 'Disabled the source : ' + str(self.model.Sources[Index][0])
+
+    def IsSrcActive(self, index):
+        return self.chkActiveSrcs[index].get()
+
+    def AddSource(self, r, c, source):
+        rd = tk.Radiobutton(self.ParentFrame,
+            text = source,
+            variable = self.SelectedSrc,
+            command = self.OnSelectSrc,
+            value = r-1)
+        rd.grid(row=r, column=c, sticky='w')
+        self.radCurSrc.append(rd)
+
+    def OnSelectSrc(self):
+        SrcIndex = self.SelectedSrc.get()
+        self.model.SrcCnf.UpdateSource(SrcIndex, False)
+        print 'Source changed to : ' + self.model.Source
+        Logger.Log('Source changed to : ' + self.model.Source)
+
+    def AddBranch(self, r, c):
+        label = UIFactory.AddLabel(self.ParentFrame, 'Branch Name Updating...', r, c)
+        self.lblBranches.append(label)
+
+    def SetBranch(self, index, branch):
+        self.lblBranches[index].set(branch)
+
+    def GetBranch(self, index):
+        return self.lblBranches[index].get()
+
+    def AddConfig(self, r, c, config):
+        configInx = ConfigEncoder.Configs.index(config)
+        combo = UIFactory.AddCombo(self.ParentFrame, ConfigEncoder.Configs, configInx, r, c, self.OnConfigChanged, r-1, 10)
+        self.cboConfig.append(combo)
+
+    def OnConfigChanged(self, row):
+        selectedInx = self.GetConfigInx(row)
+        if self.model.UpdateConfig(row, selectedInx):
+            self.model.WriteConfigFile()
+            srcTuple = self.model.Sources[row]
+            print 'Config Changed to {} for source {}'.format(srcTuple[1], srcTuple[0])
+
+    def GetConfigInx(self, row):
+        return self.cboConfig[row].current()
+
+    def AddPlatform(self, r, c, platform):
+        platformInx = ConfigEncoder.Platforms.index(platform)
+        combo = UIFactory.AddCombo(self.ParentFrame, ConfigEncoder.Platforms, platformInx, r, c, self.OnPlatformChanged, r-1, 10)
+        self.cboPlatform.append(combo)
+
+    def OnPlatformChanged(self, row):
+        selectedInx = self.GetPlatformInx(row)
+        if self.model.UpdatePlatform(row, selectedInx):
+            self.model.WriteConfigFile()
+            srcTuple = self.model.Sources[row]
+            print 'Platform Changed to {} for source {}'.format(srcTuple[2], srcTuple[0])
+
+    def GetPlatformInx(self, row):
+        return self.cboPlatform[row].current()
+
+    def DeleteRow(self, row):
+        del self.chkActiveSrcs[row]
+        del self.radCurSrc[row]
+        del self.lblBranches[row]
+        del self.cboConfig[row]
+        del self.cboPlatform[row]
+
+
 class UISourceSelector(UIWindow):
     def __init__(self, parent, model, klaRunner, vsSolutions, VM, threadHandler):
-        super(UISourceSelector, self).__init__(parent, model, 'Select Source')
+        super(UISourceSelector, self).__init__(parent, model, 'Source Manager')
         self.klaRunner = klaRunner
         self.vsSolutions = vsSolutions
         self.VM = VM
@@ -22,29 +136,10 @@ class UISourceSelector(UIWindow):
         self.srcBuilder = KlaSourceBuilder(self.model, self.klaRunner, self.vsSolutions)
 
     def CreateUI(self, parent):
-        self.SelectedSrc = tk.IntVar()
-        self.SelectedSrc.set(self.model.SrcIndex)
-        self.lblBranches = []
-        self.cboConfig = []
-        self.cboPlatform = []
-        self.chkActiveSrcs = []
         self.Row = 0
         self.mainFrame = parent
-        frameGrid = self.AddRow()
-        UIFactory.AddLabel(frameGrid, 'Is Active', 0, 0)
-        UIFactory.AddLabel(frameGrid, 'Current Source', 0, 1)
-        UIFactory.AddLabel(frameGrid, 'Branch', 0, 2)
-        UIFactory.AddLabel(frameGrid, 'Configuration', 0, 3)
-        UIFactory.AddLabel(frameGrid, 'Platform', 0, 4)
-        r = 1
-        for srcTuple in self.model.Sources:
-            self.AddActive(frameGrid, r, 0)
-            self.AddSource(frameGrid, r, 1, srcTuple[0])
-            self.AddBranch(frameGrid, r, 2, srcTuple[0])
-            self.AddConfig(frameGrid, r, 3, srcTuple[1])
-            self.AddPlatform(frameGrid, r, 4, srcTuple[2])
-            r += 1
-
+        self.SourceGrid = UISourceGrid(self.model, self.AddRow())
+        self.SourceGrid.CreateUI()
         self.AddSolutions()
         self.AddFunctions()
 
@@ -71,74 +166,17 @@ class UISourceSelector(UIWindow):
                     data.append([srcTuple[0], branch])
                 else:
                     data.append(['', brn1])
-            self.lblBranches[index].set(branch)
+            self.SourceGrid.SetBranch(index, branch)
             if index == self.model.SrcIndex:
                 self.model.Branch = branch
             index += 1
         print PrettyTable(TableFormat().SetSingleLine()).ToString(data)
 
     def OnClosing(self):
-        self.model.Branch = self.lblBranches[self.model.SrcIndex].get()
+        self.model.Branch = self.SourceGrid.GetBranch(self.model.SrcIndex)
         if self.VM is not None:
             self.VM.UpdateSourceBranch()
         super(UISourceSelector, self).OnClosing()
-
-    def AddSource(self, parent, r, c, source):
-        rd = tk.Radiobutton(parent,
-            text = source,
-            variable = self.SelectedSrc,
-            command = self.OnSelectSrc,
-            value = r-1)
-        rd.grid(row=r, column=c, sticky='w')
-
-    def OnSelectSrc(self):
-        SrcIndex = self.SelectedSrc.get()
-        self.model.SrcCnf.UpdateSource(SrcIndex, False)
-        print 'Source changed to : ' + self.model.Source
-        Logger.Log('Source changed to : ' + self.model.Source)
-
-    def AddBranch(self, parent, r, c, source):
-        label = UIFactory.AddLabel(parent, 'Branch Name Updating...', r, c)
-        self.lblBranches.append(label)
-
-    def AddConfig(self, parent, r, c, config):
-        configInx = ConfigEncoder.Configs.index(config)
-        combo = UIFactory.AddCombo(parent, ConfigEncoder.Configs, configInx, r, c, self.OnConfigChanged, r-1, 10)
-        self.cboConfig.append(combo)
-
-    def OnConfigChanged(self, row):
-        selectedInx = self.cboConfig[row].current()
-        if self.model.UpdateConfig(row, selectedInx):
-            self.model.WriteConfigFile()
-            srcTuple = self.model.Sources[row]
-            print 'Config Changed to {} for source {}'.format(srcTuple[1], srcTuple[0])
-
-    def AddPlatform(self, parent, r, c, platform):
-        platformInx = ConfigEncoder.Platforms.index(platform)
-        combo = UIFactory.AddCombo(parent, ConfigEncoder.Platforms, platformInx, r, c, self.OnPlatformChanged, r-1, 10)
-        self.cboPlatform.append(combo)
-
-    def OnPlatformChanged(self, row):
-        selectedInx = self.cboPlatform[row].current()
-        if self.model.UpdatePlatform(row, selectedInx):
-            self.model.WriteConfigFile()
-            srcTuple = self.model.Sources[row]
-            print 'Platform Changed to {} for source {}'.format(srcTuple[2], srcTuple[0])
-
-    def AddActive(self, parent, r, c):
-        Index = r - 1
-        isActive = Index in self.model.ActiveSrcs
-        chk = UIFactory.AddCheckBox(parent, '', isActive, r, c, self.OnActiveSrcChanged, (Index,), 'ew')
-        self.chkActiveSrcs.append(chk)
-
-    def OnActiveSrcChanged(self, Index):
-        if self.chkActiveSrcs[Index].get():
-            self.model.ActiveSrcs.append(Index)
-            self.model.ActiveSrcs = list(set(self.model.ActiveSrcs))
-            print 'Enabled the source : ' + str(self.model.Sources[Index][0])
-        else:
-            self.model.ActiveSrcs.remove(Index)
-            print 'Disabled the source : ' + str(self.model.Sources[Index][0])
 
     def AddSolutions(self):
         allSlns = self.vsSolutions.GetAllSlnFiles()
@@ -201,18 +239,30 @@ class UISourceSelector(UIWindow):
 
     def OnAddSource(self):
         folderSelected = tkFileDialog.askdirectory()
-        if len(folderSelected) > 0:
+        if self.IsValidSource(folderSelected):
             ConfigEncoder.AddSrc(self.model, folderSelected)
             row = len(self.model.Sources)
-            self.AddRow(row, self.model.Sources[-1])
+            srcTuple = self.model.Sources[row - 1]
+            self.SourceGrid.AddSrcRow(row, srcTuple)
             print 'New source added : ' + folderSelected
+            branch = Git.GetBranch(srcTuple[0])
+            self.SourceGrid.SetBranch(row - 1, branch)
+
+    def IsValidSource(self, newSrcDir):
+        if len(newSrcDir) == 0:
+            return False
+        for src, cnfg, pf in self.model.Sources:
+            if src == newSrcDir:
+                return False
+        return True
 
     def OnRemoveSource(self):
         src = self.model.Sources[self.model.SrcIndex][0]
         if MessageBox.YesNoQuestion('Remove Source', 'Do you want to remove source ' + src):
-            msg = 'The source ' + src + ' has been removed.'
-            del self.model.Sources[self.model.SrcIndex]
-            del self.cboConfig[self.model.SrcIndex]
-            del self.cboPlatform[self.model.SrcIndex]
-            self.model.SrcCnf.UpdateSource(self.model.SrcIndex - 1, False)
-            print msg
+            if self.model.SrcCnf.UpdateSource(self.model.SrcIndex - 1, False):
+                msg = 'The source ' + src + ' has been removed.'
+                del self.model.Sources[self.model.SrcIndex]
+                self.SourceGrid.DeleteRow(self.model.SrcIndex)
+                print msg
+            else:
+                print 'The source can not be removed.'
